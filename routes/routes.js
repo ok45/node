85cf5c62bff73ce515d8e3be04f2260989097dee
Tomáš Kalina <info@tomaskalina.cz>
2021-03-29 15:41:58 +0200

    fix request

--- routes/routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = app => {
-    const todos = require("../controllers/todoController.js");
-
-    app.post("/todos", todos.create);
-    app.get("/todos", todos.findAll);
-    app.put("/todos/:todoId", todos.update);
-    app.delete("/todos/:todoId", todos.delete);
-}
